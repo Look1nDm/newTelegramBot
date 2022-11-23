a31204939f64ea03d2d436a1d87b2f59980c80d1
dimka <dimka411841994@gmail.com>
2022-11-23 18:40:21 +0300

    Initial commit

---
+++ src/test/java/com/example/newtelegrambot/NewTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
